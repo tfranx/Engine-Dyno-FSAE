9a551753f72d5d873ea33dc9a88c08b673e58316
tfranx <tfranks.fsae@gmail.com>
2017-03-16 13:00:10 -0500

    Initial upload for absorber controller
    PWM_FORCE_Output (forces outputs at Arduino PWM headers), Lab_Code is initial code for PID control system for Telma absorber, Data sheets for Arduino Uno, MOSFETs, Telma absorber, and 12V battery.

---
+++ PWM_FORCE_Output/PWM_FORCE_Output.ino/PWM_FORCE_Output.ino.ino
@@ -0,0 +1,19 @@
+#define pwm1Pin 3
+#define pwm2Pin 5
+#define pwm3Pin 9
+#define pwm4Pin 11
+
+double dutyCycle = (25 * 2.55); //0-255, ( # * 2.55 ) = dutyCycle input
+void setup() {
+pinMode(pwm1Pin, OUTPUT);
+pinMode(pwm2Pin, OUTPUT);
+pinMode(pwm3Pin, OUTPUT);
+pinMode(pwm4Pin, OUTPUT);
+}
+
+void loop() {
+analogWrite(pwm1Pin, dutyCycle);
+analogWrite(pwm2Pin, dutyCycle);
+analogWrite(pwm3Pin, dutyCycle);
+analogWrite(pwm4Pin, dutyCycle);
+}
